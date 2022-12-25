e7129c426c66475d535b8b228350103f3f846362
Travis Xie <travisxwp@gmail.com>
2022-12-25 23:03:07 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# 构建打包前端项目镜像，别名为 builder
+# 构建阶段
+FROM node:alpine as builder
+
+WORKDIR '/app'
+
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+
+# 构建 Nginx 服务器镜像
+# 运行阶段
+FROM nginx
+# 从构建阶段完成的目录 /app/build 拷贝文件到 nginx 静态文件文件夹下
+COPY --from=builder /app/build /usr/share/nginx/html
+
+
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="nodejs.org">Learn React</a>
+        Hi There，What's up,hey 555111ss1123
+      </header>
+    </div>
+  );
+}
+
+export default App;
